252153a77ccce0636b944d284a7b5348f32eb168
Sivi2706 <137089663+Sivi2706@users.noreply.github.com>
2025-02-24 20:28:48 +0800

    Proper PWM

--- 4WD.py
+++ 4WD.py
@@ -1,6 +1,6 @@
 import RPi.GPIO as GPIO
 import time
-#Testing Raspberry pi 
+
 # Define GPIO pins for the motor driver
 IN1 = 3
 IN2 = 4
@@ -21,31 +21,72 @@ GPIO.setup(ENA, GPIO.OUT)
 GPIO.setup(ENB, GPIO.OUT)
 
 # Set up PWM for speed control
-pwmA = GPIO.PWM(ENA, 1000)
-pwmB = GPIO.PWM(ENB, 1000)
-pwmA.start(100)  # Set speed to 100%
-pwmB.start(100)  # Set speed to 100%
+pwmA = GPIO.PWM(ENA, 1000)  # 1000 Hz frequency
+pwmB = GPIO.PWM(ENB, 1000)  # 1000 Hz frequency
+pwmA.start(0)  # Start with 0% duty cycle (stopped)
+pwmB.start(0)  # Start with 0% duty cycle (stopped)
 
-def move_forward():
+def move_forward(speed):
     GPIO.output(IN1, GPIO.HIGH)
     GPIO.output(IN2, GPIO.LOW)
     GPIO.output(IN3, GPIO.HIGH)
     GPIO.output(IN4, GPIO.LOW)
-    GPIO.output(ENA, x)
-    GPIO.output(ENB. y)
+    pwmA.ChangeDutyCycle(speed)
+    pwmB.ChangeDutyCycle(speed)
 
-try:
-    move_forwar(x, y)
-    print("Moving forward")
-    time.sleep(5)  # Move forward for 5 seconds
+def move_backward(speed):
+    GPIO.output(IN1, GPIO.LOW)
+    GPIO.output(IN2, GPIO.HIGH)
+    GPIO.output(IN3, GPIO.LOW)
+    GPIO.output(IN4, GPIO.HIGH)
+    pwmA.ChangeDutyCycle(speed)
+    pwmB.ChangeDutyCycle(speed)
 
-finally:
-    # Stop the motors
+def turn_left(speed):
+    GPIO.output(IN1, GPIO.LOW)
+    GPIO.output(IN2, GPIO.HIGH)
+    GPIO.output(IN3, GPIO.HIGH)
+    GPIO.output(IN4, GPIO.LOW)
+    pwmA.ChangeDutyCycle(speed)
+    pwmB.ChangeDutyCycle(speed)
+
+def turn_right(speed):
+    GPIO.output(IN1, GPIO.HIGH)
+    GPIO.output(IN2, GPIO.LOW)
+    GPIO.output(IN3, GPIO.LOW)
+    GPIO.output(IN4, GPIO.HIGH)
+    pwmA.ChangeDutyCycle(speed)
+    pwmB.ChangeDutyCycle(speed)
+
+def stop_motors():
     GPIO.output(IN1, GPIO.LOW)
     GPIO.output(IN2, GPIO.LOW)
     GPIO.output(IN3, GPIO.LOW)
     GPIO.output(IN4, GPIO.LOW)
+    pwmA.ChangeDutyCycle(0)
+    pwmB.ChangeDutyCycle(0)
+
+try:
+    # Example usage
+    move_forward(50)  # Move forward at 50% speed
+    print("Moving forward")
+    time.sleep(5)
+
+    move_backward(30)  # Move backward at 30% speed
+    print("Moving backward")
+    time.sleep(5)
+
+    turn_left(70)  # Turn left at 70% speed
+    print("Turning left")
+    time.sleep(5)
+
+    turn_right(70)  # Turn right at 70% speed
+    print("Turning right")
+    time.sleep(5)
+
+finally:
+    # Stop the motors and clean up
+    stop_motors()
     pwmA.stop()
     pwmB.stop()
-GPIO.cleanup()
-# Testting 2 
+    GPIO.cleanup()
